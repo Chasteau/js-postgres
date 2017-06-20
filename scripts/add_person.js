4cd1019ace639d26579be30255cf5d7b56828846
Chasteau <davidchasteau@gmail.com>
2017-06-20 23:02:28 +0000

    used knex to insert

---
+++ scripts/add_person.js
@@ -0,0 +1,62 @@
+ // script that takes in the first name, last name and date
+ // of a famous person as three command line arguments
+ // and uses Knex to perform an insert.
+
+const settings = require("../settings");//settings.js
+
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+    user      : settings.user,
+    password  : settings.password,
+    database  : settings.database,
+    host      : settings.hostname,
+    port      : settings.port,
+    ssl       : settings.ssl
+    }
+});
+
+const inputParam  = process.argv.slice(2,5);
+  let firstName  = inputParam[0];
+  let lastName   = inputParam[1];
+  let bday   = inputParam[2];
+
+const printResult =  (result) => {
+  console.log(`Found ${result.rows.length} person(s) by the name '${result.rows[0].last_name}:`)
+  console.log(`- ${result.rows[0].id} ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate}'`);
+};
+
+// console.log(knex);
+
+knex('famous_people')
+  .returning('id')
+  .insert({
+    first_name: firstName,
+    last_name: lastName,
+    birthdate: bday
+  }).then((id) => {
+    console.log(id);
+  });
+
+
+
+// knex.select('*').from('famous_people')
+//   .then( (row) => {
+//     console.log(row);
+//     // printResult(user);
+//   })
+//   .catch((error)=> {
+//     console.log(error)
+//   });
+
+// knex.select().from("famous_people")
+// .where('last_name', '=', last_name)
+// .then((values) => {
+// console.log(values.rows);
+// //printResult(values)
+// })
+// .catch((error)=> {
+//   console.log(error)
+// });
+
+
