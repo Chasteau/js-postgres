2cf473ca58201f0d636f9ee2847be00129da82cf
Chasteau <davidchasteau@gmail.com>
2017-06-21 03:32:13 +0000

    successfully migrated db

--- migrations/20170620231731_create_milestones.js
+++ migrations/20170620231731_create_milestones.js
@@ -1,22 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  //  milstones table
-  // The milestones table should have the following fields:
-  // description (string)
-  // date_achieved (date)
 
-  knex.schema.createTable('milestones', function(table) {
-    return Promise.all([
-    table.increments();
-    table.string('description');
-    table.date("date_achieved");
-    table.timestamps()
-    ])
-  })
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table) {
+      table.increments();
+      table.string('description');
+      table.date("date_achieved");
+      table.timestamps()
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('milestones');
-    ])
+    knex.schema.dropTable('milestones')
+  ])
 };
